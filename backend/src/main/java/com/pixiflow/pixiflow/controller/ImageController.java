a0dbee082caefbb41bfbd979dd78dba78d531847
Aditya <officialdesaiaditya@gmail.com>
2025-11-08 21:25:24 +0530

    Integrate aws sdk

--- backend/src/main/java/com/pixiflow/pixiflow/controller/ImageController.java
+++ backend/src/main/java/com/pixiflow/pixiflow/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.pixiflow.pixiflow.controller;
 
+import com.pixiflow.pixiflow.service.AwsS3UploadService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,22 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
 
+    private final AwsS3UploadService awsS3UploadService;
+
+    public ImageController(AwsS3UploadService awsS3UploadService) {
+        this.awsS3UploadService = awsS3UploadService;
+    }
+
     @PostMapping("/upload")
     public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file) {
-        String fileName = file.getOriginalFilename();
         try {
-            file.transferTo(new File("../images/" + fileName));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            String url = awsS3UploadService.uploadFile(file);
+            return ResponseEntity.ok(url);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Some error occured");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        System.out.println(fileName);
-        return ResponseEntity.ok("File uploaded successfully");
     }
 }
---
+++ backend/src/main/java/com/pixiflow/pixiflow/service/AwsS3UploadService.java
@@ -0,0 +1,33 @@
+package com.pixiflow.pixiflow.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class AwsS3UploadService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    public AwsS3UploadService(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "-" + file.getName();
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, file.getInputStream(), null);
+        amazonS3.putObject(putObjectRequest);
+        return amazonS3.getUrl(bucketName, fileName).toString();
+    }
+}
